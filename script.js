d67e9a113be25d422f5a33ef7f942acce1865da2
CrisEspi <crisespinosa91@gmail.com>
2022-11-03 15:38:17 -0300

    prueba slider

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ background.addEventListener('click', ocultarMenu)
 function abrirMenu(){
     nav.style.right='-50px'
     background.style.display='block'
+    button.style.transform = 'rotate(180deg)'
 }
 
 function ocultarMenu(){
     nav.style.right='-250px'
     background.style.display='none'
+    button.style.transform = 'rotate(0deg)'
 }
+
 
