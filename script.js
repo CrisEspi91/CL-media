57ccf8500a01e45f0dbfb30504149b91cc02b34c
CrisEspi <crisespinosa91@gmail.com>
2022-11-09 17:49:04 -0300

    agrego js, centro en desktop

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const nav = document.getElementById('nav')
 const button = document.getElementById('nav-btn')
 const background = document.getElementById('black-nav')
@@ -18,4 +17,21 @@ function ocultarMenu(){
     button.style.transform = 'rotate(0deg)'
 }
 
+window.onscroll = function(){
+    scroll = document.documentElement.scrollTop
+    links = document.getElementsByClassName('nav-a')
+    header = document.getElementById('header')
+    logoBlanco = document.getElementById('logo-blanco')
+    logoNegro = document.getElementById('logo-negro')
 
+    if(scroll > 100){
+        header.classList.add('nav-change')
+        logoBlanco.style.display = 'none'
+        logoNegro.style.display = 'inline'
+    }else if(scroll < 20){
+        header.classList.remove('nav-change')
+        logoBlanco.style.display = 'inline'
+        logoNegro.style.display = 'none'
+
+    }
+}
